7b6fd87f62c5d048354944b085c4b7fe27c9dcf7
eweagraff <weagraff.emily@gmail.com>
2021-06-12 18:03:57 -0700

    created the server in the server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+// require express dependency
+const express = require("express");
+//const { v4: uuidv4 } = require("uuid");
+
+const routes = require("./routes/routes.js");
+
+// sets up the express app
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(routes);
+//starts server to begin listening
+app.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
